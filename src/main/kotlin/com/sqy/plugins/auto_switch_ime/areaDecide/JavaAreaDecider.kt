1a8b1a411439c2a584b84972de61193705d75644
QiuYang Shen <1741258352@qq.com>
2024-07-27 00:12:46 +0800

    java语言注释区判断优化

--- src/main/kotlin/com/sqy/plugins/auto_switch_ime/areaDecide/JavaAreaDecider.kt
+++ src/main/kotlin/com/sqy/plugins/auto_switch_ime/areaDecide/JavaAreaDecider.kt
@@ -5,67 +5,77 @@ import com.intellij.lang.java.JavaLanguage
 import com.intellij.psi.PsiComment
 import com.intellij.psi.PsiElement
 import com.intellij.psi.impl.source.tree.LeafPsiElement
-import com.intellij.psi.javadoc.PsiDocTag
+import com.intellij.psi.javadoc.PsiDocComment
 
 object JavaAreaDecider : AreaDecider {
 
     override fun isCommentArea(psiElement: PsiElement, isLineEnd: Boolean): Boolean {
-        psiElement.let {
-            it as? LeafPsiElement
-        }?.let {
-            return isCommentArea(it,isLineEnd)
+        // 单行注释区
+        if (psiElement is PsiComment) {
+            return true
         }
-        // 此处理论上不会到达
+        // 单行注释区末尾（这里会将文档注释（多行注释）的最后的空格也判断为单行注释区，暂时不做处理）
+        if (isLineEnd && psiElement.prevSibling is PsiComment) {
+            return true
+        }
+        // 文档注释区
+        if ((psiElement.parent != null && psiElement.parent is PsiDocComment)
+            || (psiElement.parent.parent != null && psiElement.parent.parent is PsiDocComment)
+            || (psiElement.parent.parent.parent != null && psiElement.parent.parent.parent is PsiDocComment)) {
+            return true
+        }
         return false
     }
 
+    @Deprecated("")
     fun isCommentArea(psiElement: LeafPsiElement, isLineEnd: Boolean): Boolean {
         // 单行注释区
         if (psiElement is PsiComment) {
             return true
         }
+        // 单行注释区末尾（这里会将文档注释（多行注释）的最后的空格也判断为单行注释区，暂时不做处理）
+        if (isLineEnd && psiElement.prevSibling is PsiComment) {
+            return true
+        }
         // 文档注释区
-        if (psiElement.toString() == "PsiDocToken:DOC_COMMENT_DATA") {
+        if ((psiElement.parent != null && psiElement.parent is PsiDocComment)
+            || (psiElement.parent.parent != null && psiElement.parent.parent is PsiDocComment)
+            || (psiElement.parent.parent.parent != null && psiElement.parent.parent.parent is PsiDocComment)) {
             return true
         }
-        if (psiElement.treePrev == null) {
-            return false
-        }
-        // 单行注释区末尾
-        if (isLineEnd && psiElement.treePrev is PsiComment) {
-            return true
-        }
-        // 文档注释区行尾
-        if (isLineEnd && psiElement.treePrev.toString() == "PsiDocToken:DOC_COMMENT_DATA") {
-            return true
-        }
-        // 文档区，行尾，前面是类似 @param xxx
-        if (isLineEnd && (psiElement.treePrev is PsiDocTag
-                    || psiElement.treePrev.treeParent is PsiDocTag)) {
-            return true
-        }
-        if (psiElement.treeNext == null) {
-            return false
-        }
-        // 文档注释区行首
-        if (psiElement.treePrev.toString() == "PsiDocToken:DOC_COMMENT_LEADING_ASTERISKS"
-            && (psiElement.treeNext.toString() == "PsiDocToken:DOC_COMMENT_LEADING_ASTERISKS"
-                        || psiElement.treeNext.toString() == "PsiDocToken:DOC_COMMENT_END")) {
-            return true
-        }
+//        if (psiElement.toString() == "PsiDocToken:DOC_COMMENT_DATA") {
+//            return true
+//        }
+//        if (psiElement.treePrev == null) {
+//            return false
+//        }
+//
+//        // 文档注释区行尾
+//        if (isLineEnd && psiElement.treePrev.toString() == "PsiDocToken:DOC_COMMENT_DATA") {
+//            return true
+//        }
+//        // 文档区，行尾，前面是类似 @param xxx
+//        if (isLineEnd && (psiElement.treePrev is PsiDocTag
+//                    || psiElement.treePrev.treeParent is PsiDocTag)) {
+//            return true
+//        }
+//        if (psiElement.treeNext == null) {
+//            return false
+//        }
+//        // 文档注释区行首
+//        if (psiElement.treePrev.toString() == "PsiDocToken:DOC_COMMENT_LEADING_ASTERISKS"
+//            && (psiElement.treeNext.toString() == "PsiDocToken:DOC_COMMENT_LEADING_ASTERISKS"
+//                        || psiElement.treeNext.toString() == "PsiDocToken:DOC_COMMENT_END")) {
+//            return true
+//        }
         return false
     }
 
     override fun isCodeArea(psiElement: PsiElement, isLineEnd: Boolean): Boolean {
-        psiElement.let {
-            it as? LeafPsiElement
-        }?.let {
-            return isCodeArea(it,isLineEnd)
-        }
-        // 此处应该永远不会到达
-        throw Error("插件内部错误!")
+        return true
     }
 
+    @Deprecated("")
     fun isCodeArea(psiElement: LeafPsiElement, isLineEnd: Boolean): Boolean {
 //        if (psiElement is PsiIdentifier
 //            || psiElement.treeParent is PsiExpression
